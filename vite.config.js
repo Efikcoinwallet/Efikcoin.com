bce20186c7f0eef1b3ac4d1aad4eabf2ff15858d
Wallet <efikcoin@gmail.com>
2026-08-10 16:04:22 +0200

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',  // For custom domain, this is fine
+  build: {
+    outDir: 'dist'
+  }
+})
